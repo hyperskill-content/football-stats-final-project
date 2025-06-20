1d56b80a183222093d71313c1640b75cade47fc6
Evancelj <w.lexandra@gmail.com>
2025-06-20 13:31:33 +0300

    Issue#17: updated Person class

--- src/main/java/com/hyperskill/data_models/Person.java
+++ src/main/java/com/hyperskill/data_models/Person.java
@@ -1,8 +1,12 @@
 package com.hyperskill.data_models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public abstract class Person {
     private String firstName;
     private String lastName;
+    private String teamName;
 
     public Person() {
     }
@@ -10,6 +14,12 @@ public abstract class Person {
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, String teamName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.teamName = teamName;
     }
 
     public String getFirstName() {
@@ -20,12 +30,20 @@ public abstract class Person {
         return lastName;
     }
 
+    public String getTeamName() {
+        return teamName;
+    }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
     }
 }
 
